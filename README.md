e41b27c6212ebfdd8511e9118234728e88fc0d05
yaohsiao <yaohsiaopid@gapp.nthu.edu.tw>
2019-02-19 12:57:18 +0800

    add read

---
+++ README.md
@@ -0,0 +1,2 @@
+## Underway: Follow https://events.linuxfoundation.org/wp-content/uploads/2017/12/Enabling-Zephyr-on-Your-Hardware-Platform-Diego-Sueiro-Sepura-Embarcados.pdf 
+
---
+++ setup.sh
@@ -0,0 +1,5 @@
+source zephyr-env.sh    
+export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
+export GNUARMEMB_TOOLCHAIN_PATH=/usr/local/gcc-arm-none-eabi-7-2018-q2-update/
+source activate cs231n
+
